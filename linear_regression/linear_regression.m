771e6eb4c157717d890143c27a91fc387d15eddf
liuyanan <oceanwave@yeah.net>
2016-05-09 15:46:56 +0800

    添加了简单的用noraml equation 计算的linear regresion的例子

---
+++ linear_regression/linear_regression.m
@@ -0,0 +1,44 @@
+% predict the temperature by calculating the rate at which crickets chirp
+% 通过蟋蟀鸣叫的速率来预测温度
+
+% load data from text file
+% column1: temperature column2: chirps
+data = load('cricket_chirps_versus_temperature.txt');
+data
+
+% Define x and y
+% x the feature, we have only one 
+% y is the output, as this is supervisor leanring
+% c(1,:) % will print all headers;         c(2:end,:) % will all  data rows
+x = data(:,2);
+y = data(:,1);
+
+
+function plotData(x, y)
+    plot(x, y, 'rx', 'MarkerSize', 8);
+    xlabel('Rate of Cricket Chirping'); % Set the x-axis label
+    ylabel('Temperature in Degrees Fahrenheit'); % Set the y-axis label
+    fprintf('Program paused. Press enter to continue.\n');
+end
+
+plotData(x, y);
+pause;
+
+
+% Count how many data points we have
+m = length(x);
+% Add a column of all ones (intercept term) to x
+% 这个新加的 [1, .... , 1]T 是 x0, 初始值是1
+X = [ones(m, 1) x];
+
+
+% calculte Theta
+theta = (pinv(X' * X)) * X' * y;
+% theta = [24.9660; 3.3058]
+% 所以我们的hypothesis 为 y = 24.9660 + 3.3058 * x
+
+
+hold on; % this keeps our previous plot of the training data visible
+plot(X(:,2), X*theta, '-')
+legend('Training data', 'Linear regression')
+hold off % Don't put any more plots on this figure
